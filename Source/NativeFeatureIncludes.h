277f555aaa1ecc2f5c975836986a60508426d8f3
Gie "Max" Vanommeslaeghe <gievanom@hotmail.com>
2023-04-18 16:02:21 +0200

    Updated defaults to not include libcat

--- Source/NativeFeatureIncludes.h
+++ Source/NativeFeatureIncludes.h
@@ -51,7 +51,7 @@
 
 // SET DEFAULTS IF UNDEFINED
 #ifndef LIBCAT_SECURITY
-#define LIBCAT_SECURITY 1
+#define LIBCAT_SECURITY 0
 #endif
 #ifndef _RAKNET_SUPPORT_ConnectionGraph2
 #define _RAKNET_SUPPORT_ConnectionGraph2 1
